aee5e51649651515134bf539cafd67fba177a96e
YugiHandif25 <mochamadyugi@gmail.com>
2022-09-07 11:52:06 +0700

    /

--- mrp_request/models/workorder_daily.py
+++ mrp_request/models/workorder_daily.py
@@ -7,8 +7,19 @@ class WorkorderDaily(models.Model):
     employee_id = fields.Many2one('hr.employee', string='Operator',)
     date        = fields.Date(string='Date', default=fields.Date.today())
     mrp_workorder_line_ids = fields.One2many('mrp.workorder.line', 'wo_daily_id', 'Line')
+    scanner     = fields.Char('Scanner')
 
     @api.model
     def barcode_scan(self, active_id):
         action = self.env.ref('mrp_request.workorder_daily_wizard_action').read()[0]
         return action
+    
+    @api.onchange('scanner')
+    def _onchange_qr_code(self):
+       if self.scanner:
+            return {
+                'warning' : {
+                    'title' : 'Success',
+                    'message' : 'Hasil Scanner %s' % (self.scanner)
+                }
+            }
