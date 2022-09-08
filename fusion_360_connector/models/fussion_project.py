47e7729afda5c01ae712a6df274e017e9c10c88c
YugiHandif25 <mochamadyugi@gmail.com>
2022-09-08 08:08:22 +0700

    Fusion 360 detail

--- fusion_360_connector/models/fussion_project.py
+++ fusion_360_connector/models/fussion_project.py
@@ -5,9 +5,13 @@ class FusionProject(models.Model):
 
     name = fields.Char(string='Name')
     id_fp = fields.Char(string='Id Fp')
+    detail_line_ids = fields.One2many('fusion.project.line', 'fusion_project_id', 'Line')
+
 
 
 class FusionProjectLine(models.Model):
     _name = 'fusion.project.line'
 
     name = fields.Char(string='Name')
+    product_id = fields.Many2one('product.product', string='Product')
+    fusion_project_id = fields.Many2one('fusion.project', 'Invoice')
