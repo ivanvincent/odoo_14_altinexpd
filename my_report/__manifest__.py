7edc880ec0c7f9294d7a893e0b81419c712c14f3
Fidyandh <fidya.syafiyah@gmail.com>
2022-10-13 16:57:38 +0700

    [NEW][FIX] bill_invoice_without_payment

--- my_report/__manifest__.py
+++ my_report/__manifest__.py
@@ -42,7 +42,8 @@
         # 'report/packing_list_pengiriman_kain.xml',
         # 'report/konstruksi_design.xml',
         'report/print_kartu_proses.xml',
-        'report/bill_invoice_with_payment.xml',
+        'report/bill_invoice_with_payment.xml'
+        'report/bill_invoice_without_payment.xml',
         
         
         # 'views/inherit_stock.xml',
