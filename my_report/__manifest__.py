7c983df733db263269ef8957530700123d4e02bc
Fidyandh <fidya.syafiyah@gmail.com>
2022-10-13 16:10:17 +0700

    [new] bill_invoice_with_payment

--- my_report/__manifest__.py
+++ my_report/__manifest__.py
@@ -41,7 +41,8 @@
         # 'report/bukti_pengeluaran_greige.xml',
         # 'report/packing_list_pengiriman_kain.xml',
         # 'report/konstruksi_design.xml',
-        'report/print_kartu_proses.xml'
+        'report/print_kartu_proses.xml',
+        'report/bill_invoice_with_payment.xml',
         
         
         # 'views/inherit_stock.xml',
