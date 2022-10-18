2c24e0dfd2f20dd52b2d767f2bcdac6f31d274fc
Lutvi Rizki Nurbaiti <lutvirizki@student.telkomuniversity.ac.id>
2022-10-18 11:08:22 +0700

    /

--- inherit_inventory/models/__init__.py
+++ inherit_inventory/models/__init__.py
@@ -2,7 +2,7 @@ from . import stock_picking
 from . import stock_move
 from . import stock_move_image
 from . import account_move
-# from . import stock_move_line
+from . import stock_move_line
 # from . import stock_warehouse
 # from . import product_attribute
 # from . import product_category
