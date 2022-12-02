755427397397ea68b7e3152fe320fea1bbc79477
YugiHandif25 <mochamadyugi@gmail.com>
2022-12-02 10:34:46 +0700

    fix waiting time attendance

--- inherit_hr/models/hr_employee_base.py
+++ inherit_hr/models/hr_employee_base.py
@@ -17,11 +17,10 @@ class HrEmployeeBase(models.AbstractModel):
             vals = {
                 'employee_id': self.id,
                 'check_in': action_date,
-                'time_waiting': action_date + timedelta(minutes=30),
             }
             return self.env['hr.attendance'].create(vals)
         attendance = self.env['hr.attendance'].search([('employee_id', '=', self.id), ('check_out', '=', False)], limit=1)
-        # self.check_waiting_time(attendance)
+        self.check_waiting_time(attendance, action_date)
         if attendance:
             attendance.check_out = action_date
         else:
@@ -30,6 +29,6 @@ class HrEmployeeBase(models.AbstractModel):
         return attendance
     # End Override From base odoo
 
-    def check_waiting_time(self, attendance):
-        if attendance.check_in <= attendance.time_waiting:
+    def check_waiting_time(self, attendance, date_now):
+        if date_now <= attendance.time_waiting:
             raise exceptions.UserError('Thank you, you have already checked in before')
