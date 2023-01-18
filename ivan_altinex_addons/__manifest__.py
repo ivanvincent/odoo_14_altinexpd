6ba81ab36de67996080b16a91f90b8730e3423da
YugiHandif25 <mochamadyugi@gmail.com>
2023-01-18 14:35:56 +0700

    /

--- ivan_altinex_addons/__manifest__.py
+++ ivan_altinex_addons/__manifest__.py
@@ -9,9 +9,9 @@
     """,
     # data files always loaded at installation
     'data': [
-        # 'views/hr_contract.xml',
-        # 'views/hr_employee.xml',
-        # 'views/product_views.xml',
+        'views/hr_contract.xml',
+        'views/hr_employee.xml',
+        'views/product_views.xml',
     ],
     # data files containing optionally loaded demonstration data
 }
