b28035abef24cf02261d6672c82d4f64d54f0869
YugiHandif25 <mochamadyugi@gmail.com>
2023-01-18 13:10:42 +0700

    /

--- hr_employee_updation/models/hr_employee.py
+++ hr_employee_updation/models/hr_employee.py
@@ -37,7 +37,7 @@ class HrEmployeeFamilyInfo(models.Model):
     employee_id = fields.Many2one('hr.employee', string="Employee", help='Select corresponding Employee',
                                   invisible=1)
     relation_id = fields.Many2one('hr.employee.relation', string="Relation", help="Relationship with the employee")
-         = fields.Char(string='Name')
+    member_name = fields.Char(string='Name')
     member_contact = fields.Char(string='Contact No')
     birth_date = fields.Date(string="DOB", tracking=True)
 
