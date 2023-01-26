c1f4c83b408d531b353c1b723b7c92c3215e61c4
YugiHandif25 <mochamadyugi@gmail.com>
2023-01-26 11:27:21 +0700

    waiting time check out

--- inherit_hr/models/hr_attendance.py
+++ inherit_hr/models/hr_attendance.py
@@ -6,8 +6,13 @@ class HrAttendance(models.Model):
     _inherit = 'hr.attendance'
 
     time_waiting = fields.Datetime(string='Waiting Check In', compute='compute_time_waiting')
+    time_waiting_co = fields.Datetime(string='Waiting Check Out', compute='compute_time_waiting')
 
-    @api.depends('check_in')
+    @api.depends('check_in', 'check_out')
     def compute_time_waiting(self):
         for rec in self:
-            rec.time_waiting = rec.check_in + timedelta(minutes=30)
+            rec.time_waiting = rec.check_in + timedelta(minutes=1)
+            if rec.check_out:
+                rec.time_waiting_co = rec.check_out + timedelta(minutes=1)
+            else:
+                rec.time_waiting_co = False
