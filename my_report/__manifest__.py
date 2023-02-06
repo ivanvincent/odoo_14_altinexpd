8eaa6a8809645c7949e614f85668d6ed85069dcf
Fidyandh <fidya.syafiyah@gmail.com>
2023-02-06 14:52:31 +0700

    [ADD] manifest

--- my_report/__manifest__.py
+++ my_report/__manifest__.py
@@ -51,6 +51,7 @@
         'report/report_staff.xml',
         'report/laporan_sales_contract.xml',
         'report/print_manufacturing_request.xml',
+        'report/report_salary_to_gs.xml',
 
         
         # 'views/inherit_stock.xml',
