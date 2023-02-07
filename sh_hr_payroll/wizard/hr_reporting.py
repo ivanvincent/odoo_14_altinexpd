6a305c8a1f697e076a510abf952f243baa876edc
Fidyandh <fidya.syafiyah@gmail.com>
2023-02-07 17:59:19 +0700

    [ADD] report_salary_to_bpjs : tambah button, temp

--- sh_hr_payroll/wizard/hr_reporting.py
+++ sh_hr_payroll/wizard/hr_reporting.py
@@ -3,6 +3,7 @@
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
+from datetime import date, datetime, timedelta
 
 
 class HrReporting(models.TransientModel):
@@ -13,5 +14,39 @@ class HrReporting(models.TransientModel):
     date_start = fields.Date(string="Start date", required=True)
     date_end = fields.Date(string="End date", required=True)
 
+    def action_generate_pdf(self):
+        date_start_tmp  = datetime.strptime(self.date_start.strftime('%Y-%m-%d') + ' 00:00:00', '%Y-%m-%d %H:%M:%S')
+        date_start      = date_start_tmp - timedelta(hours=7)
+        
+        date_end_tmp    = datetime.strptime(self.date_end.strftime('%Y-%m-%d') + ' 23:59:59', '%Y-%m-%d %H:%M:%S')
+        date_end        = date_end_tmp - timedelta(hours=7)
 
-   
+        # % (date_start, date_end, self.kelompok)
+
+        # self._cr.execute(query)
+        # result = self._cr.dictfetchall()
+        
+        data = {
+            'me': self,
+            'ids': self.ids,
+            'model': self._name,
+            'form': {
+                'date_start': self.date_start,
+                'date_end': self.date_end,
+                # 'data' : result,
+            },
+        }
+        return self.env.ref('sh_hr_payroll.action_report_salary_bpjs').report_action(None, data=data)
+
+class HrReporting(models.AbstractModel):
+    _name = 'report.sh_hr_payroll.report_salary_to_bpjs'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        # print('_get_report_values')
+        date_start = data['form']['date_start']
+        date_end = data['form']['date_end']
+        return {
+            'date_start': date_start,
+            'date_end': date_end,
+        }
