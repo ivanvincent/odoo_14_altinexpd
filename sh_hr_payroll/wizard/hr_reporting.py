873ae3d0dda46239695aa9620fdf87f8b9190db9
Fidyandh <fidya.syafiyah@gmail.com>
2023-02-07 18:04:00 +0700

    [FIX ERROR]

--- sh_hr_payroll/wizard/hr_reporting.py
+++ sh_hr_payroll/wizard/hr_reporting.py
@@ -15,16 +15,6 @@ class HrReporting(models.TransientModel):
     date_end = fields.Date(string="End date", required=True)
 
     def action_generate_pdf(self):
-        date_start_tmp  = datetime.strptime(self.date_start.strftime('%Y-%m-%d') + ' 00:00:00', '%Y-%m-%d %H:%M:%S')
-        date_start      = date_start_tmp - timedelta(hours=7)
-        
-        date_end_tmp    = datetime.strptime(self.date_end.strftime('%Y-%m-%d') + ' 23:59:59', '%Y-%m-%d %H:%M:%S')
-        date_end        = date_end_tmp - timedelta(hours=7)
-
-        # % (date_start, date_end, self.kelompok)
-
-        # self._cr.execute(query)
-        # result = self._cr.dictfetchall()
         
         data = {
             'me': self,
@@ -39,7 +29,7 @@ class HrReporting(models.TransientModel):
         return self.env.ref('sh_hr_payroll.action_report_salary_bpjs').report_action(None, data=data)
 
 class HrReporting(models.AbstractModel):
-    _name = 'report.sh_hr_payroll.report_salary_to_bpjs'
+    _name = 'report.sh_hr_payroll.report_salary_bpjs'
 
     @api.model
     def _get_report_values(self, docids, data=None):
