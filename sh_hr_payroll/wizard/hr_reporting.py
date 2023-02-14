eeb59ce1e524823c313ed67760bc8d188dcde416
Fidyandh <fidya.syafiyah@gmail.com>
2023-02-14 16:50:02 +0700

    gaji pokok

--- sh_hr_payroll/wizard/hr_reporting.py
+++ sh_hr_payroll/wizard/hr_reporting.py
@@ -20,27 +20,28 @@ class HrReporting(models.TransientModel):
     def action_generate_pdf(self):
 
         if self.report_type == 'bpjs': 
-            query = """
+            query =f"""
             SELECT he.name, sum(payslip.total_gapok) as total_gapok, sum(payslip.KES) as total_kes
             FROM (
                     SELECT hpl.code, hpl.total as total_gapok, 0 as KES, hp.employee_id
                     FROM
                     hr_payslip hp left join hr_payslip_line hpl on hpl.slip_id = hp.id
                     WHERE hpl.code = 'GAPOK'
-                    AND hp.date_from >= %s AND hp.date_to <= %s 
+                    AND hp.date_from >= '{self.date_start}' AND hp.date_to <= '{self.date_start}' 
 
                     UNION
                     SELECT hpl.code, 0 as total_gapok, hpl.total as KES, hp.employee_id as karyawan
                     FROM 
                     hr_payslip hp left join hr_payslip_line hpl on hpl.slip_id = hp.id
                     WHERE hpl. code = 'KES'
-                    AND hp.date_from >= %s AND hp.date_to <= %s
+                    AND hp.date_from >= '{self.date_start}' AND hp.date_to <= '{self.date_start}'
                 ) AS payslip 
             left join hr_employee he on he.id = payslip.employee_id
             GROUP BY he.name
-            """ % (self.date_start, self.date_end)
+            """ 
+            print("action_print")
             self._cr.execute(query)
-            result = self._cr.dictfetchall()
+            record = self._cr.dictfetchall()
             
             data = {
                 'me': self,
@@ -49,7 +50,7 @@ class HrReporting(models.TransientModel):
                 'form': {
                     'date_start': self.date_start,
                     'date_end': self.date_end,
-                    'data' : result,
+                    'record' : record,
                 },
             }
             return self.env.ref('sh_hr_payroll.action_report_salary_bpjs').report_action(None, data=data)
@@ -75,12 +76,14 @@ class HrReportingBpjs(models.AbstractModel):
         print('_get_report_values')
         date_start = data['form']['date_start']
         date_end = data['form']['date_end']
-        docs = data['form']['data']
+        record = data['form']['record']
         return {
+            'me': self,
             'date_start': date_start,
             'date_end': date_end,
             'doc_ids': data['ids'],
-            'docs': docs,
+            'docs': record,
+            
         }
 
 class HrReportingGS(models.AbstractModel):
