7f593d4857dd607c0560b18f58b6f813a49e38ea
YugiHandif25 <mochamadyugi@gmail.com>
2023-03-02 12:53:31 +0700

    Fix Laporan Shift 3

--- inherit_hr/wizard/laporan_shift_3.py
+++ inherit_hr/wizard/laporan_shift_3.py
@@ -7,8 +7,8 @@ class LaporanShiftTigaWizard(models.TransientModel):
     date_start = fields.Date(string='Date Start', required=True, )
     date_end = fields.Date(string='Date End', required=True, )
 
-    def action_generate(self):
+    def action_generate(self):        
         action = self.env.ref('inherit_hr.hr_attendance_pivot_shift_3_action').read()[0]        
         action['domain'] = [('shift_3_counter', '>', 0), ('check_in', '>=', self.date_start),
-                            ('check_in', '<=', self.date_end)]
+                            ('check_out', '<', self.date_end)]
         return action
