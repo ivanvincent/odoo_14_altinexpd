da6eb56ae1dd9e5684fa9afc6e5fee78fe00ac21
YugiHandif25 <mochamadyugi@gmail.com>
2023-03-21 14:58:36 +0700

    /

--- hr_employee_updation/models/hr_employee.py
+++ hr_employee_updation/models/hr_employee.py
@@ -153,7 +153,7 @@ class HrEmployeePublic(models.Model):
     anak_ke = fields.Char(string='Product name')
     batch_recruitment  = fields.Integer(string="Batch Recruitment", readonly=True)
     contract_type_id = fields.Many2one(related='contract_id.type_id', string='Contract')
-    shift_id         = fields.Many2one('hr.employee.shift', string='Shift')
+    # shift_id         = fields.Many2one('hr.employee.shift', string='Shift')
     golongan_id      = fields.Many2one('hr.employee.golongan', string='Golongan')
     kelompok_id      = fields.Many2one('hr.employee.kelompok', string='Kelompok')
     grop_id          = fields.Many2one('hr.employee.grop', string='Group')
