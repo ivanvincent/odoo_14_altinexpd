e59ccaf57cce5d92796fe2486fc6f5df12e75f6b
Fidyandh <fidya.syafiyah@gmail.com>
2023-03-27 11:46:32 +0700

    add parent workcenter

--- mrp_request/models/mrp_workcenter_category.py
+++ mrp_request/models/mrp_workcenter_category.py
@@ -4,3 +4,7 @@ class MrpWorkcenterCategory(models.Model):
     _name = 'mrp.workcenter.category'
 
     name = fields.Char(string='Name')
+    parent_category = fields.Selection([
+    ('mentah', 'MENTAH'),
+    ('matang', 'MATANG')],
+    string='Parent Category')
