ab279863a9a54eb09d9ecd3dcf5d8aa021da6279
YugiHandif25 <mochamadyugi@gmail.com>
2023-04-20 12:20:10 +0700

    /

--- wibicon_altinex_sale/models/sale_order.py
+++ wibicon_altinex_sale/models/sale_order.py
@@ -11,7 +11,7 @@ class SaleOrder(models.Model):
     note_so         = fields.Char(string='Note')
     no_dqc          = fields.Boolean(related='partner_id.no_dqc', string='Status DQC')
     # payment_term_id = fields.Many2one(related='quotation_id.payment_term_id', string='Payment Term')
-    payment_term_id = fields.Many2one('account.payment.term', string='Payment Term')
+    # payment_term_id = fields.Many2one('account.payment.term', string='Payment Term')
     # demantional_quality_control
 
     def action_confirm(self):
@@ -40,40 +40,39 @@ class SaleOrder(models.Model):
         # print("action_create_mor")
 
     # super(SaleOrder, self).action_cancel()
-    def onchange_partner_id(self) :
-        res = super(SaleOrder, self).onchange_partner_id()
+    # def onchange_partner_id(self) :
+    #     res = super(SaleOrder, self).onchange_partner_id()
 
-        if not self.partner_id:
-            self.update({
-                'partner_invoice_id': False,
-                'partner_shipping_id': False,
-                'fiscal_position_id': False,
-            })
-            return
+    #     if not self.partner_id:
+    #         self.update({
+    #             'partner_invoice_id': False,
+    #             'partner_shipping_id': False,
+    #             'fiscal_position_id': False,
+    #         })
+    #         return
 
-        self = self.with_company(self.company_id)
+    #     self = self.with_company(self.company_id)
 
-        addr = self.partner_id.address_get(['delivery', 'invoice'])
-        partner_user = self.partner_id.user_id or self.partner_id.commercial_partner_id.user_id
-        values = {
-            'pricelist_id': self.partner_id.property_product_pricelist and self.partner_id.property_product_pricelist.id or False,
-            'payment_term_id': self.partner_id.property_payment_term_id and self.partner_id.property_payment_term_id.id or False,
-            'partner_invoice_id': addr['invoice'],
-            'partner_shipping_id': addr['delivery'],
-        }
-        user_id = partner_user.id
-        if not self.env.context.get('not_self_saleperson'):
-            user_id = user_id or self.env.context.get('default_user_id', self.env.uid)
-        if user_id and self.user_id.id != user_id:
-            values['user_id'] = user_id
+    #     addr = self.partner_id.address_get(['delivery', 'invoice'])
+    #     partner_user = self.partner_id.user_id or self.partner_id.commercial_partner_id.user_id
+    #     values = {
+    #         'pricelist_id': self.partner_id.property_product_pricelist and self.partner_id.property_product_pricelist.id or False,
+    #         'payment_term_id': self.partner_id.property_payment_term_id and self.partner_id.property_payment_term_id.id or False,
+    #         'partner_invoice_id': addr['invoice'],
+    #         'partner_shipping_id': addr['delivery'],
+    #     }
+    #     user_id = partner_user.id
+    #     if not self.env.context.get('not_self_saleperson'):
+    #         user_id = user_id or self.env.context.get('default_user_id', self.env.uid)
+    #     if user_id and self.user_id.id != user_id:
+    #         values['user_id'] = user_id
 
-        if self.env['ir.config_parameter'].sudo().get_param('account.use_invoice_terms') and self.env.company.invoice_terms:
-            values['note'] = self.with_context(lang=self.partner_id.lang).env.company.invoice_terms
-        if not self.env.context.get('not_self_saleperson') or not self.team_id:
-            values['team_id'] = self.env['crm.team'].with_context(
-                default_team_id=self.partner_id.team_id.id
-            )._get_default_team_id(domain=['|', ('company_id', '=', self.company_id.id), ('company_id', '=', False)], user_id=user_id)
-        self.update(values)
-
-        return res
+    #     if self.env['ir.config_parameter'].sudo().get_param('account.use_invoice_terms') and self.env.company.invoice_terms:
+    #         values['note'] = self.with_context(lang=self.partner_id.lang).env.company.invoice_terms
+    #     if not self.env.context.get('not_self_saleperson') or not self.team_id:
+    #         values['team_id'] = self.env['crm.team'].with_context(
+    #             default_team_id=self.partner_id.team_id.id
+    #         )._get_default_team_id(domain=['|', ('company_id', '=', self.company_id.id), ('company_id', '=', False)], user_id=user_id)
+    #     self.update(values)
+    #     return res
     
