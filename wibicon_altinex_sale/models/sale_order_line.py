02463612ff715f1e7e1777e403a275a93780fdfa
YugiHandif25 <mochamadyugi@gmail.com>
2023-05-03 16:22:30 +0700

    Update-an sales

--- wibicon_altinex_sale/models/sale_order_line.py
+++ wibicon_altinex_sale/models/sale_order_line.py
@@ -1,11 +1,15 @@
 from odoo import models, fields, api
 from odoo.addons import decimal_precision as dp
 
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    treatment_id    = fields.Many2one('treatment', string='Heat Treatment')
-    shape           = fields.Selection([("caplet","Caplet"),("round","Round")], string='Shape')
-    kd_bahan        = fields.Char('Kode Bahan')
-    lapisan         = fields.Selection([("coating","Coating"),("plating","Plating")], string='Surface Finish')
-    tax_id          = fields.Many2many(string='Tax')
+    treatment_id = fields.Many2one('treatment', string='Heat Treatment')
+    shape = fields.Selection(
+        [("caplet", "Caplet"), ("round", "Round")], string='Shape')
+    kd_bahan = fields.Char('Kode Bahan')
+    lapisan = fields.Selection(
+        [("Coat", "Coat"), ("Plat", "Plat")], string='Surface Finish')
+    tax_id = fields.Many2many(string='Tax')
+    embos = fields.Char(string='Embos')
