6883d51d115a5bcfc4b61d5b5afb62269fcab825
Tanjung <tanjung.omni@gmail.com>
2023-06-28 18:24:17 +0700

    [FIX] date order harusya ke delivery date

--- sale_contract/models/sale_order.py
+++ sale_contract/models/sale_order.py
@@ -128,18 +128,18 @@ class SaleOrderContract(models.Model):
             'context': self.env.context,
             }
 
-    # @api.onchange('date_order')
-    # def onchange_date_order_date(self):
+    @api.onchange('date_order')
+    def onchange_date_order_date(self):
 
-    #     tahun = int(self.date_order.strftime("%Y"))
-    #     bulan = int(self.date_order.strftime("%m"))
-    #     tgl = int(self.date_order.strftime("%d"))
-    @api.onchange('delivery_date')
-    def onchange_date_delivery_date(self):
+        tahun = int(self.date_order.strftime("%Y"))
+        bulan = int(self.date_order.strftime("%m"))
+        tgl = int(self.date_order.strftime("%d"))
+    # @api.onchange('delivery_date')
+    # def onchange_date_delivery_date(self):
 
-        tahun = int(self.delivery_date.strftime("%Y"))
-        bulan = int(self.delivery_date.strftime("%m"))
-        tgl = int(self.delivery_date.strftime("%d"))
+    #     tahun = int(self.delivery_date.strftime("%Y"))
+    #     bulan = int(self.delivery_date.strftime("%m"))
+    #     tgl = int(self.delivery_date.strftime("%d"))
         
         tmp_desc = self.get_current_week(tahun,bulan,str(tgl))
         tmp_out = []
