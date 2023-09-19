4ef0f33a772a51d750234e327320c06f4c98bcbd
Tanjung <tanjung.omni@gmail.com>
2023-09-19 17:07:10 +0700

    [FIX] field master spesifikasi

--- master_specifications/models/specifications.py
+++ master_specifications/models/specifications.py
@@ -5,6 +5,7 @@ class Specifications(models.Model):
 
     jenis_id = fields.Many2one('master.jenis', string='Jenis')
     spec_id = fields.Many2one('master.require', string='Spec',domain="[('jenis_ids', 'in', jenis_id)]")
+    spect_name = fields.Char(string='Nama Spefisikasi')
     desc = fields.Char(string='Desc')
     harga = fields.Float(string='Harga')
-    active = fields.Boolean(string='Active ?')
+    active = fields.Boolean(string='Active ?', Default=True)
