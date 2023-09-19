3ae32e799fee46f6c4703c3aac0a33cd5408986c
Tanjung <tanjung.omni@gmail.com>
2023-09-19 21:44:47 +0700

    /

--- master_specifications/models/quotation_request_form.py
+++ master_specifications/models/quotation_request_form.py
@@ -236,9 +236,10 @@ class QuotationRequestFormLine(models.Model):
     line_spec_ids = fields.One2many('quotation.request.form.line.specification', 'qrf_line_id', 'Line Spec')
     name = fields.Char(string='Description')
     quantity = fields.Float(string='Quantity')
-    price_unit = fields.Float(string='Price Unit', compute='_compute_price_unit')
+    price_unit = fields.Float(string='Price Unit')
+    # compute='_compute_price_unit'
     tax_ids = fields.Many2many(comodel_name='account.tax', string='Tax')
-    sub_total = fields.Float(string='Sub Total', compute='_compute_price_unit')
+    sub_total = fields.Float(string='Sub Total', compute='_compute_sub_total')
     state = fields.Selection(
         [("draft", "Draft"), ("confirm", "Confirm")], string='State', default='draft')
 
@@ -260,7 +261,7 @@ class QuotationRequestFormLine(models.Model):
     @api.depends('line_spec_ids', 'price_unit' , 'sub_total')
     def compute_price_unit(self):
         for rec in self:
-            tot_price =+ rec.line_spec_ids.harga
+            tot_price += rec.line_spec_ids.harga
             self.price_unit = tot_price
             exclude = self.quantity * tot_price
             self.sub_total = exclude
