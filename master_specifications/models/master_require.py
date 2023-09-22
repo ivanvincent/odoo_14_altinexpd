51a44bac2fb0df1f178a253cb48b4ad5ebd3159b
lutvirizki <lutvirizki@student.telkomuniversity.ac.id>
2023-09-22 10:55:01 +0700

    compute jenis_ids master require

--- master_specifications/models/master_require.py
+++ master_specifications/models/master_require.py
@@ -6,7 +6,14 @@ class MasterRequire(models.Model):
     name = fields.Char(string='Name')
     jenis_ids = fields.Many2many(
         comodel_name='master.jenis', 
-        string='Jenis'
+        string='Jenis',
+        compute="_get_jenis_ids"
         )
     active = fields.Boolean(string='Active ?', default=True)
+
+
+    def _get_jenis_ids(self):
+            for line in self:
+                spec = self.env['specifications'].search([('spec_id', '=', line.id)])
+                line.jenis_ids = [(6, 0, spec.mapped('jenis_id.id'))]
     
