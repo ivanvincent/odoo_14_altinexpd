fb00397ccbeb809467235605462ca5ce05c0ebad
YugiHandif25 <mochamadyugi@gmail.com>
2023-10-18 15:58:29 +0700

    /

--- web_custom/models/res_partner.py
+++ web_custom/models/res_partner.py
@@ -4,11 +4,11 @@ from odoo.exceptions import UserError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    address            = fields.Char(string='Address', readonly=False, store=False,)
-    nama_perusahaan    = fields.Char(string='Nama Perusahaan')
-    jabatan            = fields.Char(string='Jabatan')
-    divisi             = fields.Char(string='Divisi')
-    gender_user        = fields.Selection([("male","Male"),("female","Female")], string='Gender', readonly=False, store=False)
+    address            = fields.Char(string='Address', readonly=False, store=False, compute='compute_info')
+    nama_perusahaan    = fields.Char(string='Nama Perusahaan', compute='compute_info')
+    jabatan            = fields.Char(string='Jabatan', compute='compute_info')
+    divisi             = fields.Char(string='Divisi', compute='compute_info')
+    gender_user        = fields.Selection([("male","Male"),("female","Female")], string='Gender', readonly=False, store=False, compute='compute_info')
 
     def compute_info(self):
         for rec in self:
