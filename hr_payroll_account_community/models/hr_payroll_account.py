abb3d956ebcf92adbfb8c146a3ffb1bb23c78ca8
lutvirizki <lutvirizki@student.telkomuniversity.ac.id>
2023-10-21 15:20:21 +0700

    /

--- hr_payroll_account_community/models/hr_payroll_account.py
+++ hr_payroll_account_community/models/hr_payroll_account.py
@@ -29,7 +29,7 @@ class HrPayslip(models.Model):
 
     date = fields.Date('Date Account', states={'draft': [('readonly', False)]}, readonly=True,
                        help="Keep empty to use the period of the validation(Payslip) date.")
-    journal_id = fields.Many2one('account.journal', 'Salary Journal', readonly=True, required=True,
+    journal_id = fields.Many2one('account.journal', 'Salary Journal', readonly=True,
                                  states={'draft': [('readonly', False)]},
                                  default=lambda self: self.env['account.journal'].search([('type', '=', 'general')],
                                                                                          limit=1))
