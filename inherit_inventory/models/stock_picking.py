55aa6b634c813fc654464d16fd2440c4d49e05d7
Ivan Vincent <ivan.vincent@gmail.com>
2023-12-09 22:36:50 +0700

    fix state label in picking

--- inherit_inventory/models/stock_picking.py
+++ inherit_inventory/models/stock_picking.py
@@ -18,7 +18,7 @@ class StockPicking(models.Model):
         ('draft', 'Draft'),
         ('waiting', 'Waiting Another Operation'),
         ('confirmed', 'Waiting'),
-        ('assigned', 'Assigned'),
+        ('assigned', 'Partial'),
         ('done', 'Done'),
         ('cancel', 'Cancelled'),
     ], string='Status', compute='_compute_state',
