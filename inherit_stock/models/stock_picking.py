e92ede7e1cae9c635341f0107e63aa9e76ab1767
lutvirizki <lutvirizki@student.telkomuniversity.ac.id>
2023-12-19 16:00:47 +0700

    /

--- inherit_stock/models/stock_picking.py
+++ inherit_stock/models/stock_picking.py
@@ -228,7 +228,8 @@ class StockPicking(models.Model):
 
     def button_validate(self):
         res = super(StockPicking, self).button_validate()
-        self.date_done = self.scheduled_date #date done diset sesuai dengan schedule date
+        self.date_done = datetime.now()
+        # self.date_done = self.scheduled_date #date done diset sesuai dengan schedule date
         if res == True:
             if sum(self.move_line_ids_without_package.mapped('waste')) > 0:
                 self._create_picking_scrap()
